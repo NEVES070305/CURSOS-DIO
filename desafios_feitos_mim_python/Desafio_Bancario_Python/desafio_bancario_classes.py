760b28c84089a37b98ab9c5d5e2c96c311bd9d92
NEVES <guilhermenevesdasilva321@gmail.com>
2023-06-21 23:51:58 -0300

    Classes, contas, verificacao de transações

--- desafios_feitos_mim_python/Desafio_Bancario_Python/desafio_bancario_classes.py
+++ desafios_feitos_mim_python/Desafio_Bancario_Python/desafio_bancario_classes.py
@@ -27,7 +27,7 @@ class Conta:
         self._numero=numero
         self._agencia=agencia
         self._cliente=cliente
-        self.historico=Historico()
+        self._historico=Historico()
 
     @classmethod
     def nova_conta(cls,cliente,numero):
@@ -40,11 +40,51 @@ class Conta:
     @property
     def numero(self):
         return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    def sacar(self,valor):
+        saldo=self.saldo
+        excedeu_saldo=valor>saldo
+
+        if excedeu_saldo:
+            print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+
+        elif valor>0:
+            self._saldo-=valor
+            print("\nSaque realizado com sucesso!")
+            return True
+
+        else:
+            print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+
+        return False
+    
+
+
 
 
 
 class ContaCorrente(Conta):
-    pass
+    def __init__(self, saldo, numero, agencia, cliente,limite=500,limite_saques=3):
+        super().__init__(saldo, numero, agencia, cliente)
+        self.limite=limite
+        self.limite_saques=limite_saques
+
+    def sacar(self,valor):
+        numero_saques=len(
+            [transacao for transacao in self.historico.transacoes if transacao["tipo"]==Saque.__name__]
+        )
 
 class Historico:
     pass
@@ -57,4 +97,5 @@ class Saque(Transacao):
 
 class Deposito(Transacao):
     pass
+
 
